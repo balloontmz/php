301c8ce2298084cb2556d4e1fd3cc44dacf1459d
tomtiddler <tomtiddler@tomtiddlerdeMacBook-Pro.local>
2019-08-09 17:45:25 +0800

    对象池，php 中应该不可用

---
+++ design-patterns/src/Creational/Pool/Tests/PoolTest.php
@@ -0,0 +1,27 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use DesignPatterns\Creational\Pool\WorkPool;
+
+class PoolTest extends TestCase
+{
+    public function testCanGetNewInstancesWithGet()
+    {
+        $pool = new WorkPool();
+        $worker1 = $pool->get();
+        $worker2 = $pool->get();
+
+        $this->assertCount(2, $pool);
+        $this->assertNotSame($worker1, $worker2);
+    }
+
+    public function testCanGetSameInstanceTwiceWhenDisposingItFirst()
+    {
+        $pool = new WorkPool();
+        $worker1 = $pool->get();
+        $pool->dispose($worker1);
+        $worker2 = $pool->get();
+
+        $this->assertCount(1, $pool);
+        $this->assertSame($worker1, $worker2);
+    }
+}
---
+++ design-patterns/src/Creational/Pool/WorkPool.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace DesignPatterns\Creational\Pool;
+
+/**
+ * 由于 php 的单线程特性，该模式应该不可用才对，猜测对于常驻内存的语言才好用
+ */
+class WorkPool implements \Countable
+{
+    private $occupiedWorkers = [];
+
+    private $freeWorkers = [];
+
+    public function get(): StringReverseWorker
+    {
+        if (count($this->freeWorkers) == 0) {
+            $worker = new StringReverseWorker();
+        } else {
+            $worker = array_pop($this->freeWorkers);
+        }
+
+        $this->occupiedWorkers[spl_object_hash($worker)] = $worker;
+
+        return $worker;
+    }
+
+    public function dispose(StringReverseWorker $worker)
+    {
+        $key = spl_object_hash($worker);
+        if (isset($this->occupiedWorkers[$key])) {
+            unset($this->occupiedWorkers[$key]);
+            $this->freeWorkers[$key] = $worker;
+        }
+    }
+
+    public function count(): int
+    {
+        return count($this->occupiedWorkers) + count($this->freeWorkers);
+    }
+}
