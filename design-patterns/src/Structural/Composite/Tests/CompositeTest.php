4bded13d2a44e1fab8d86b890daa6080d782e0e5
tomtiddler <tomtiddler@tomtiddlerdeMacBook-Pro.local>
2019-08-13 11:25:03 +0800

    组合模式

---
+++ design-patterns/src/Structural/Composite/Tests/CompositeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DesignPatterns\Structural\Composite\Tests;
+
+use DesignPatterns\Structural\Composite\Form;
+use DesignPatterns\Structural\Composite\InputElement;
+use DesignPatterns\Structural\Composite\TextElement;
+use PHPUnit\Framework\TestCase;
+
+class CompositeTest extends TestCase
+{
+    public function testRender()
+    {
+        $form = new Form();
+        $form->addElement(new TextElement('Email:'));
+        $form->addElement(new InputElement());
+        $embed = new Form();
+        $embed->addElement(new TextElement('Password:'));
+        $embed->addElement(new InputElement());
+        $form->addElement($embed);
+
+        $this->assertEquals(
+            '<form>Email:<input type="text" /><form>Password:<input type="text" /></form></form>',
+            $form->render()
+        );
+    }
+}
