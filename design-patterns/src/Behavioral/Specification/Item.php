7c8304d33949952917d2ca2c7ad2af3eac00b5c9
tomtiddler <tomtiddler@tomtiddlerdeMacBook-Pro.local>
2019-08-16 18:40:11 +0800

    规格模式

---
+++ design-patterns/src/Behavioral/Specification/Item.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace DesignPatterns\Behavioral\Specification;
+
+class Item
+{
+    private $price;
+
+    public function __construct(float $price)
+    {
+        $this->price = $price;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+}
